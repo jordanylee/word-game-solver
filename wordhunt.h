f967f3cc2dd4658f06f214fbd1a45affc5f63e07
jordanylee <146499047+jordanylee@users.noreply.github.com>
2023-12-17 18:53:49 -0600

    Update wordhunt.h

--- wordhunt.h
+++ wordhunt.h
@@ -1,5 +1,10 @@
-#define SIZE 4
+extern int SIZEBOARD;
 #define N 26
+
+typedef struct CreatedWord {
+    char input[17];
+    int length;
+} Word;
 
 typedef struct GameTile {
     char letter;
@@ -10,31 +15,17 @@ typedef struct GameBoard {
     Tile tiles[4][4];
 } Board;
 
-typedef struct CreatedWord {
-    char input[17];
-    int length;
-} Word;
+typedef struct TrieNode {
+    struct TrieNode* children[N];
+    bool leaf;
+    bool found;
+} TrieNode;
 
-typedef struct TrieNode TrieNode;
-
-void wordFind(Board board, Word word, int row, int col, TrieNode* root, int* counter, Word* foundWords);
-
-void printBoard(Board board);
-
+TrieNode* initializeDictionary(void);
+TrieNode* makeNode(void);
+void insertWord(TrieNode* root, const char* word);
 Board getBoard(void);
-
-TrieNode* makeNode(char letter);
-
-void freeTrie(TrieNode* node);
-
-TrieNode* insertWord(TrieNode* root, char* word);
-
-int searchWord(TrieNode* root, char* word);
-
-void printTrie(TrieNode* root);
-
-void printSearch(TrieNode* root, char* word);
-
-TrieNode* initDict(void);
-
+void printBoard(const Board board);
+void findWords(Board board, Word word, int row, int col, TrieNode* root, int* counter, Word* foundWords);
+int searchWord(TrieNode* root, const char* word);
 int wordCompare(const void *wordA, const void *wordB);
